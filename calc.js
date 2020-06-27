0ce3a76f6288927c011654eb9a8455d32553bd34
asifahmed786 <asifahmed1us1@gmail.com>
2020-06-27 16:39:46 +0500

    completed

--- calc.js
+++ calc.js
@@ -1,26 +1,40 @@
 document.getElementById("input1").value = "0";
-para1 = 0;
-para2 = 0;
-operationStatus = false;
-lastOperator = ""
+var para1 = 0;
+var para2 = 0;
+var operationStatus = false;
+var lastOperator = ""
+var clickStatus = false;
 
 // for numeric button press if zero then no digit prcoeding the zero 
 function press1(str) {
     var a = document.getElementById("input1");
-    if(operationStatus==true || a.value=="0"){
+    if(clickStatus==false){
+        a.value = "0"
+    }
+    else{
+
+    }
+
+
+    if(a.value=="0"){        
         a.value = str;
     }
     else{
         a.value += str;
-
     }
-
-    // if(a.value=="0"){        
-    //     a.value = str;
-    // }
-    // else{
-    //     a.value += str;
-    // }
+    clickStatus = true;
+    
+}
+function pressDot(){
+    var a = document.getElementById("input1");
+    var str1 = a.value.toString();
+    if(str1.indexOf(".")!=-1){        
+        // a.value = str;
+    }
+    else{
+        a.value += ".";
+    }
+    // clickStatus = true;
     
 }
 
@@ -39,10 +53,14 @@ function remove() {
         else {
             display2 += display1[i]
         }
-
-
     }
-    a.value = display2
+    if(display2===""){
+        a.value = "0"
+    }
+    else{
+        a.value = display2
+    }
+    
 }
 
 
@@ -50,46 +68,64 @@ function remove() {
 
 // recording the status of operation
 function operationStart(operator1) {
+    clickStatus = false;
     var a = document.getElementById("input1");
     if (operationStatus == false) {
-        para1 = parseInt(a.value);
+        para1 = parseFloat(a.value);
         a.value = "0";
         operationStatus = true;
+        lastOperator = operator1;
         if (operator1 == "=") {
 
         }
         else {
-            lastOperator = operator1;
-
+            // lastOperator = operator1;
         }        
     }
     else {
-        para2 = parseInt(a.value);
+        if (operator1 == "=") {
+
+        }
+        else {
+            // lastOperator = operator1;
+
+        }        
+        para2 = parseFloat(a.value);
         // operationStatus = false;
         if (lastOperator == "+") {
-            a.value = para1 + para2;
-            para1 = para1 + para2;;
+            // lastoperator = operator1;
+            para1 = parseFloat(para1 + para2);
+            a.value = para1;            
             para2 = 0;
             // lastoperator = "";
         }
         else if (lastOperator == "-") {
-            a.value = para1 - para2;
-            para1 = 0;
+            // lastoperator = operator1;
+            para1 = parseFloat(para1 - para2);
+            a.value = para1;            
             para2 = 0;
             // lastoperator = "";
 
         }
         else if (lastOperator == "X") {
+            // lastoperator = operator1;
             a.value = para1 * para2;
-            para1 = 0;
+            para1 = para1 * para2;;
             para2 = 0;
+            // lastoperator = "";
+        }   
+        else if (lastOperator == "/") {
+            // lastoperator = operator1;
+            para1 = parseFloat(para1 / para2);
+            a.value = para1
+            para2 = 0;            
             // lastoperator = "";
         }
-        else if (lastOperator == "/") {
-            a.value = para1 / para2;
+        else if (opeartor1=="%"){
+            lastoperator = operator1;
             para1 = 0;
             para2 = 0;
-            // lastoperator = "";
+            a.value = para1/100;
         }
     }
     
